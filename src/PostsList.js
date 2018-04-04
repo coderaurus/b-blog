986bbebf37f68244de62df960833ffb1f0681967
CodeRaurus <coderaurus@gmail.com>
2018-04-04 17:07:07 +0300

    add basic functionality add Post class add PostsList class

--- src/PostsList.js
+++ src/PostsList.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import Post from './Post.js';
 
 class PostsList extends Component {
 
     render() {
-
+        return (
+        <table id="listOfPosts">
+            <tbody>
+                <tr>
+                    <Post mode="edit"></Post>
+                </tr>
+                <tr>
+                    <Post mode="read"></Post>
+                </tr>
+            </tbody>
+        </table>);
     }
 }
+
+export default PostsList;
